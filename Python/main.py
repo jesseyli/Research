9a25863f6b6860a935e8f18e88d87d8495f4f05e
Jesse <jesseyli@berkeley.edu>
2015-07-28 13:54:51 -0700

    Commit #3, finished overlay, need to thicken lines

--- Python/main.py
+++ Python/main.py
@@ -1,28 +1,33 @@
 from PIL import Image, ImageDraw
+import ast, math
 
 imageName = input('Enter image name: ')
 img = Image.open(imageName)
+img = img.split()[0]
 dim = img.size
-degrees = int(input('Enter number of degrees per slice, must be a factor of 360: '))
+center = ast.literal_eval(input('Enter location of center (x,y): '))
+degrees = int(input('Enter number of degrees per slice, factor of 360 for best results: '))
 
-def draw_circle(size, d):
-	# Draws a circle divided into equal slices.  Each size is d degrees of the
-	# original circle.  The outputted image has dimensions size.
+def draw_circle(size, d, center):
+	# Draws a circle centered at center divided into equal slices.  Each slice is
+	# d degrees of the original circle.  The outputted image has dimensions size.
 	
 	circle = Image.new('L',size)
+	radii = min(size[0] - center[0],size[1] - center[0],size[1] - center[1],size[1] - center[0])
 	draw = ImageDraw.Draw(circle)
 	slices = 360//d
 	color = 100
-	curr_d = 0
 	for i in range(0,slices):
 		color = 300 - color
-		draw.pieslice((0,0,size[0],size[1]),i*d,i*d,
+		draw.pieslice((center[0]-radii,center[1]-radii,center[0]+radii,center[1]+radii),i*d,i*d,
 			None,color)
 
 	return circle
 
-circle = draw_circle(dim, degrees)
+circle = draw_circle(dim, degrees, center)
 overlay = Image.new('L',dim)
 for i in range(0,dim[0]):
 	for j in range(0,dim[1]):
-		#finish for loop that adds the two images together
+		overlay.putpixel((i,j),min(255,img.getpixel((i,j))+circle.getpixel((i,j))))
+
+overlay.show()
