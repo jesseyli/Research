e69c29ac2cd58487d9234be1feb320caaaa2566f
Jesse <jesseyli@berkeley.edu>
2015-08-02 22:07:54 -0700

    Able to calculate angles > 18

--- Python/main.py
+++ Python/main.py
@@ -13,7 +13,7 @@ dim = img.size
 #center = ast.literal_eval(input('Enter location of center (x,y): '))
 center = (310,305) #remove later
 #degrees = int(input('Enter number of degrees per slice, factor of 360 for best results: '))
-degrees = 90 #remove later
+degrees = 18 #remove later
 
 
 
@@ -101,32 +101,84 @@ def ends(start, img, color):
 	s8 = (s5[0],s5[1]+1)
 	s9 = (s5[0]+1,s5[1]+1)
 	if start_color == color:
-		return [start]
+		return start
 	if start_color != 255 and start_color != 1:
 		img.putpixel(start,255)
 		return ends(s1,img,color) + ends(s2,img,color) + ends(s3,img,color) + ends(s4,img,color) + ends(s6,img,color) + ends(s7,img,color) + ends(s8,img,color) + ends(s9,img,color)
-	return []
+	return ()
 	
+def distance(xcoord1, xcoord2, ycoord1, ycoord2):
+	deltax=math.pow((xcoord2-xcoord1), 2)
+	deltay=math.pow((ycoord2-ycoord1), 2)
+	distanceA=math.sqrt(deltax+deltay)
+	return distanceA
+
+
+def lawOfCosines(point1,point2,center):
+	xcoord1 = point1[0]
+	xcoord2 = point2[0]
+	originX = center[0]
+	ycoord1 = point1[1]
+	ycoord2 = point2[1]
+	originY = center[1]
+	distanceA=distance(xcoord1, xcoord2, ycoord1, ycoord2)
+	distanceB=distance(xcoord2, originX, ycoord2, originY)
+	distanceC=distance(xcoord1, originX, ycoord1, originY)
+	aSquared=math.pow(distanceA,2)
+	bSquared=math.pow(distanceB,2)
+	cSquared=math.pow(distanceC,2)
+	cosAng=((cSquared-aSquared-bSquared)/(-2*distanceA*distanceB))
+	angRad=math.acos(cosAng)
+	angDeg=(180*angRad)/math.pi
+	return angDeg
+
+
 x = center[0]
 y = center[1]
-
+angles = []
 
 while x < dim[0]:
 	value = overlay.getpixel((x,y)) 
 	if not(value == 0 or value == 255):
 		overlay.putpixel((x,y),0)
-		print((x,y),ends((x,y),overlay,2))
+		e = ends((x,y),overlay,2)
+		if e != ():
+			angles.append(lawOfCosines((x,y),e,center))
 	x += 1
 
 i = 2
 while i < last_color:
 	i += 1
 	for j in start[i-1]:
-		print(j, ends(j,overlay,i))
+		e = ends(j,overlay,i)
+		if e != ():
+			angles.append(lawOfCosines(j,e,center))
 for j in start[last_color]:
-	print(j,ends(j,overlay,1))
+	e = ends(j,overlay,1)
+	if e != ():
+		angles.append(lawOfCosines(j,e,center))
 
-img.show()
 overlay.show()
+print(len(angles))
 #for i in start:
 #	print(i,start[i])
+
+'''
+//get results from table and measure angle
+originX=254;
+originY=281;
+angles=newArray();
+
+for(i=0;i<1374;i+=2){
+	xcoord1=getResult("X",i);
+	xcoord2=getResult("X",i+1);
+	ycoord1=getResult("Y",i);
+	ycoord2=getResult("Y",i+1);
+	distanceA=distance(xcoord1, xcoord2, ycoord1, ycoord2);
+	distanceB=distance(xcoord2, originX, ycoord2, originY);
+	distanceC=distance(xcoord1, originX, ycoord1, originY);
+	angle=lawOfCosines(distanceA, distanceB, distanceC);
+	print(angle);
+}
+
+'''
