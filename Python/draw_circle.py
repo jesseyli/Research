f831919d4d53ab96311fa3ef19ccfdcaf58f7ee0
Jesse <jesseyli@berkeley.edu>
2015-08-04 13:19:56 -0700

    Works for all angles except 1 degree

--- Python/draw_circle.py
+++ Python/draw_circle.py
@@ -2,10 +2,12 @@ from PIL import Image, ImageDraw
 
 imageName = input('Enter image name: ')
 img = Image.open(imageName)
+img = img.split()[0]
 circle = Image.new('L',img.size)
 
 index = 0
 draw = ImageDraw.Draw(circle)
 draw.pieslice((0,0,circle.size[0],circle.size[1]),0,45,None,100)
 draw.pieslice((0,0,circle.size[0],circle.size[1]),90,135,None,200)
-data = list(draw.getdata())
+data = list(img.getdata())
+print(len(data))
--- Python/main.py
+++ Python/main.py
@@ -4,6 +4,7 @@ import ast, math
 # Use a branch treshold for how many pixels to travel before counting as branching
 # may not need to use branching if angle is sufficiently small
 # currently doesn't work for 1 degree increment, not enough colors with 8 bit
+# recursive algorithm no good, too many iterations, 12 degrees works but 15 doesnt?
 
 #imageName = input('Enter image name: ')
 imageName = 'nocenter.tif'  #remove later
@@ -13,7 +14,7 @@ dim = img.size
 #center = ast.literal_eval(input('Enter location of center (x,y): '))
 center = (310,305) #remove later
 #degrees = int(input('Enter number of degrees per slice, factor of 360 for best results: '))
-degrees = 18 #remove later
+degrees = 90 #remove later
 
 
 
@@ -48,13 +49,13 @@ def draw_circle(size, d, center):
 	y2 = center[1]
 
 	for i in range(0,slices):
-		color += 1
+		color = color + 1
 		radians=d*i*math.pi/180;
 		x1=radii*math.cos(radians) + x2;
 		y1=radii*math.sin(radians) + y2;
 
 		# thickness of line
-		thick = 1
+		thick = 2
 
 		# compute angle
 		a = math.pi/2
@@ -90,6 +91,19 @@ for i in range(0,dim[0]):
 			start[color].append((i,j))
 
 def ends(start, img, color):
+	'''
+	curr_coord = start
+	start_color = img.getpixel(start)
+	curr_color = start_color
+	while curr_color != color or curr_color == 255:
+		if img.getpixel()
+		# finish this, trace lines by iterating thru points
+
+	if curr_color = 255:
+		return ()
+	return curr_coord
+	'''
+
 	start_color = img.getpixel(start)
 	s5 = list(start)
 	s1 = (s5[0]-1,s5[1]-1)
@@ -102,11 +116,12 @@ def ends(start, img, color):
 	s9 = (s5[0]+1,s5[1]+1)
 	if start_color == color:
 		return start
-	if start_color != 255 and start_color != 1:
+#	if start_color != 1 and start_color != 255:
+	if start_color != 255:
 		img.putpixel(start,255)
 		return ends(s1,img,color) + ends(s2,img,color) + ends(s3,img,color) + ends(s4,img,color) + ends(s6,img,color) + ends(s7,img,color) + ends(s8,img,color) + ends(s9,img,color)
-	return ()
-	
+	return ()		
+
 def distance(xcoord1, xcoord2, ycoord1, ycoord2):
 	deltax=math.pow((xcoord2-xcoord1), 2)
 	deltay=math.pow((ycoord2-ycoord1), 2)
@@ -132,6 +147,7 @@ def lawOfCosines(point1,point2,center):
 	angDeg=(180*angRad)/math.pi
 	return angDeg
 
+overlay.save('output.png')
 
 x = center[0]
 y = center[1]
@@ -140,7 +156,7 @@ angles = []
 while x < dim[0]:
 	value = overlay.getpixel((x,y)) 
 	if not(value == 0 or value == 255):
-		overlay.putpixel((x,y),0)
+		overlay.putpixel((x,y-1),255)
 		e = ends((x,y),overlay,2)
 		if e != ():
 			angles.append(lawOfCosines((x,y),e,center))
@@ -148,20 +164,24 @@ while x < dim[0]:
 
 i = 2
 while i < last_color:
-	i += 1
-	for j in start[i-1]:
-		e = ends(j,overlay,i)
+	for j in start[i]:
+		e = ends(j,overlay,i+1)
 		if e != ():
 			angles.append(lawOfCosines(j,e,center))
+	i += 1
+
+for j in start[1]:
+	overlay.putpixel(j,1)
+
 for j in start[last_color]:
 	e = ends(j,overlay,1)
 	if e != ():
 		angles.append(lawOfCosines(j,e,center))
 
-overlay.show()
+print(sum(angles)/len(angles))
 print(len(angles))
-#for i in start:
 #	print(i,start[i])
+#for i in start:
 
 '''
 //get results from table and measure angle
